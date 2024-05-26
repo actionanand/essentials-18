1df00f7682c9ae3ef76dc09274492f460261d9d1
Anand Raja <meetanandns@gmail.com>
2024-05-27 00:32:05 +0530

    :chore: Remove commented out code in header component spec

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,8 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+// import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
+  /*
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
@@ -19,4 +20,5 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  */
 });
